f9b8c29abcf7439601bb06e9a7911850295fec22
Juan Carlos Villegas Castillo <juan.villegas@proagroindustria.com>
2026-01-12 09:34:33 -0600

    Fix: Robust subfolder routing in root index.php and .htaccess

--- index.php
+++ index.php
@@ -1,22 +1,10 @@
 <?php
 /**
- * Laravel - A PHP Framework For Web Artisans
- *
- * @package  Laravel
- * @author   Taylor Otwell <taylor@laravel.com>
- * 
- * Proxy para despliegue en subcarpetas de Hostinger.
+ * Laravel - Subfolder entry point proxy for Hostinger.
  */
 
-$uri = urldecode(
-    parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
-);
-
-// Quitamos el prefijo de la subcarpeta si existe para que file_exists funcione bien
-$publicUri = str_replace('/VECODE', '', $uri);
-
-if ($publicUri !== '/' && file_exists(__DIR__ . '/public' . $publicUri)) {
-    return false;
-}
+// Fix path variables for subfolder hosting
+$_SERVER['SCRIPT_NAME'] = '/VECODE/index.php';
+$_SERVER['PHP_SELF'] = '/VECODE/index.php';
 
 require_once __DIR__ . '/public/index.php';
