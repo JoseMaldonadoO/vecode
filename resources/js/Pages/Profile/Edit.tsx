5c24857dab99ccfbb2ae8ae4c1dfb7e8b7413665
Juan Carlos Villegas Castillo <juan.villegas@proagroindustria.com>
2026-01-12 10:22:17 -0600

    Fix: Corrected syntax error in Profile Edit page

--- resources/js/Pages/Profile/Edit.tsx
+++ resources/js/Pages/Profile/Edit.tsx
@@ -5,7 +5,8 @@ import DeleteUserForm from './Partials/DeleteUserForm';
 import UpdatePasswordForm from './Partials/UpdatePasswordForm';
 import UpdateProfileInformationForm from './Partials/UpdateProfileInformationForm';
 
-auth,
+export default function Edit({
+    auth,
     mustVerifyEmail,
     status,
 }: PageProps<{ mustVerifyEmail: boolean; status?: string }>) {
