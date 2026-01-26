1f1968d192a30f194b1373f8f6c4ca60edf2fe63
Juan Carlos Villegas Castillo <juan.villegas@proagroindustria.com>
2026-01-26 10:21:34 -0600

    Fix frontend build: remove remaining date input from Sales/Create

--- resources/js/Pages/Sales/Create.tsx
+++ resources/js/Pages/Sales/Create.tsx
@@ -131,18 +131,6 @@ export default function Create({ auth, clients, products, suggested_folios, defa
                                     )}
                                 </div>
 
-                                <div>
-                                    <label className="block text-sm font-medium text-gray-700 mb-1">Fecha</label>
-                                    <div className="relative">
-                                        <input
-                                            type="date"
-                                            value={data.date}
-                                            onChange={e => setData('date', e.target.value)}
-                                            className="w-full rounded-lg border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 py-2.5 pl-10"
-                                        />
-                                        <Calendar className="w-5 h-5 text-gray-400 absolute left-3 top-2.5" />
-                                    </div>
-                                </div>
 
                                 <div className="col-span-1 md:col-span-2">
                                     <label className="block text-sm font-medium text-gray-700 mb-1">Orden de Compra / Referencia</label>
