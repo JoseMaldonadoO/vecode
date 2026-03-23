674db9f1d4b6047f36888a9e82bba4545883f0e4
Juan Carlos Villegas Castillo <juan.villegas@proagroindustria.com>
2026-03-23 09:49:14 -0600

    feat: adjust sandbox to support traditional PHP/SQL development using Blade

--- app/Http/Controllers/SandboxController.php
+++ app/Http/Controllers/SandboxController.php
@@ -3,19 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Inertia\Inertia;
 
 class SandboxController extends Controller
 {
-    /**
-     * Display the sandbox index page.
-     */
+
     public function index(Request $request)
     {
-        return Inertia::render('Sandbox/Index', [
+
+        return view('sandbox.index', [
             'message' => '¡Bienvenido al entorno de pruebas (Sandbox)!'
         ]);
     }
 
-    // Add more test methods here...
+    // puede agregar más métodos y devolver más vistas aquí.
 }
